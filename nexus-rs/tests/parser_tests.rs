7a2eb9978c1611434694bdec82b6f1a9db036688
Kris van Rens <krisvanrens@gmail.com>
2023-06-04 23:36:29 +0200

    CHG: Make test code part of the testing routine.

--- nexus-rs/tests/parser_tests.rs
+++ nexus-rs/tests/parser_tests.rs
@@ -1,18 +1,27 @@
-use std::fs;
+use std::{ffi::OsStr, fs, path::Path};
 
 use nexus_rs::{parser::Parser, scanner::Scanner};
 
-const CODE_PATH: &str = "tests/test_code/test.nxs";
+const CODE_PATH: &str = "tests/test_code/";
 
+/// Scan `CODE_PATH` for Nexus source files, and run scanner + parser for each of them.
 #[test]
 fn parser_test() {
-    let code = fs::read_to_string(CODE_PATH)
-        .unwrap_or_else(|e| panic!("{e}"))
-        .trim()
-        .to_owned();
+    for entry in fs::read_dir(CODE_PATH).unwrap_or_else(|e| panic!("{e}")) {
+        let filename = entry.expect("invalid directory entry").file_name();
+        if Path::new(&filename).extension().and_then(OsStr::to_str) == Some("nxs") {
+            let code = fs::read_to_string(CODE_PATH.to_owned() + filename.to_str().unwrap())
+                .unwrap_or_else(|e| panic!("{e}"))
+                .trim()
+                .to_owned();
 
-    let mut scanner = Scanner::new();
-    let mut parser = Parser::new(scanner.scan(code).unwrap());
+            println!("Parsing {filename:?}..");
 
-    println!("{}", parser.parse());
+            let mut scanner = Scanner::new();
+            let mut parser = Parser::new(scanner.scan(code).unwrap());
+            parser.parse();
+
+            println!("..done");
+        }
+    }
 }
